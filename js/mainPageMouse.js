6af168ea058ddcd84e2a7a93d25896cb15a2505e
cfairborn23 <cfairborn23@my.whitworth.edu>
2024-03-06 23:05:57 -0800

    uhaelw9ufhbawliefubnawekifbnzaw,uiefbewai

--- js/mainPageMouse.js
+++ js/mainPageMouse.js
@@ -8,31 +8,32 @@ function pageEffect(e) {
         const mouseX = e.clientX;
 
         let carFrame = Math.round(45 * (mouseX / screen.width));
-        let vacFrame = Math.round(90 * (mouseX / screen.width));
+        //let vacFrame = Math.round(90 * (mouseX / screen.width));
         // let whichFrame = Math.round(45 * (vacElem.offsetTop / window.screen.height));
         if (carFrame === 0) {
             carFrame = 1;
         }
-        if (vacFrame === 0) {
-            vacFrame = 1;
-        }
+        // if (vacFrame === 0) {
+        //     vacFrame = 1;
+        // }
 
         let carUrl = "img/truckPngs/" + carFrame.toString() + ".png";
-        let vacUrl = "img/vacPngs/" + vacFrame.toString() + ".png";
+        // let vacUrl = "img/vacPngs/" + vacFrame.toString() + ".png";
 
         const newCarImg = new Image();
         newCarImg.src = carUrl;
         newCarImg.style.display = 'none';
 
-        const newVacImg = new Image();
-        newVacImg.src = vacUrl;
-        newVacImg.style.display = 'none';
+        // const newVacImg = new Image();
+        // newVacImg.src = vacUrl;
+        // newVacImg.style.display = 'none';
 
         newCarImg.onload = function() {
-            newVacImg.onload = function() {
-                document.getElementById("carAnim").innerHTML = "<img src='" + carUrl + "' />";
-                document.getElementById("vacAnim").innerHTML = "<img src='" + vacUrl + "' />";
-            };
+            document.getElementById("carAnim").innerHTML = "<img src='" + carUrl + "' />";
+            // newVacImg.onload = function() {
+            //     document.getElementById("carAnim").innerHTML = "<img src='" + carUrl + "' />";
+            //     document.getElementById("vacAnim").innerHTML = "<img src='" + vacUrl + "' />";
+            //};
         };
         
         lastExT = curT;
